06475a1c51c25384b2f49aa527ce1c6e5f1ff70b
meghana-naidu17 <meghana.naidu1707@gmail.com>
2026-02-23 10:43:04 +0530

    before deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";   // 👈 ADD THIS
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>   {/* 👈 WRAP APP */}
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Signup',
   plugins: [react()],
+  base: '/Signup/'   // 👈 VERY IMPORTANT
 })
